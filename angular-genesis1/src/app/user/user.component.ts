634d3ab91e3de200b3fc875467dccdeca581482c
Lenovo User Station <ramonjoseph.c@gmail.com>
2024-07-17 13:16:56 +0200

    Demo ready with header and acting with random users as in Max 20

---
+++ angular-genesis1/src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+/* Function to return a random index based in the length's array 
+ */
+
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+/* To make dynamic text rendering from a Jason file add a property  in the component .ts file as below
+ to the class ,and access it accordingly from the component .html file  
+ */
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+}
