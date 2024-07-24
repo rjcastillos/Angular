e29e69ffb8df669cfc463a97e1b32ef11323be7d
Lenovo User Station <ramonjoseph.c@gmail.com>
2024-07-24 10:56:43 +0200

    V0.0.7 contains demo working with users skeleton and passing the event to draw the additional pop up up to Max lesson 34

--- angular-genesis1/src/app/user/user.component.ts
+++ angular-genesis1/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 
 /* Function to return a random index based in the length's array
@@ -14,14 +14,16 @@ import { Component, Input} from '@angular/core';
 })
 
 export class UserComponent {
-@Input() avatar!: string;
-@Input() name!: string;
+@Input({ required:true}) id!: string;
+@Input({required:true}) avatar!: string;
+@Input({required:true}) name!: string;
+@Output() select = new EventEmitter( );
 
 get imagePath() {
   return 'assets/users/' + this.avatar;
 }
 
 onSelectUser (){
-
+  this.select.emit(this.id);
 }
 }
