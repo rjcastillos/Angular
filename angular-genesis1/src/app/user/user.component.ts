49a79f43ec691a97c8ad350229eef44e4c3d5248
Lenovo User Station <ramonjoseph.c@gmail.com>
2024-07-26 13:52:57 +0200

    showing in user component User as an object type or as an Interface

--- angular-genesis1/src/app/user/user.component.ts
+++ angular-genesis1/src/app/user/user.component.ts
@@ -1,5 +1,16 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
 
+/* type User = {
+  id: string;
+  avatar: string;
+  name: string;
+} */
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 /* Function to return a random index based in the length's array
  */
@@ -23,11 +34,7 @@ export class UserComponent {
 //  this.avatar to this.user.avatar
 //
 //
-@Input({required: true}) user!: {
-  id: string;
-  avatar: string;
-  name: string;
-};
+@Input({required: true}) user!: User;
 @Output() select = new EventEmitter<string>();
 
 get imagePath() {
